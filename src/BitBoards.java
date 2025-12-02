982efcd6a32a4a042e60e4b9bb780d5c4b027a38
Rakhman Indarbiev <rindarbie@gmail.com>
2025-12-02 23:02:47 +0100

    BitBoards move function

--- src/BitBoards.java
+++ src/BitBoards.java
@@ -7,9 +7,23 @@ public class BitBoards {
                                 0x0008L, 0x0800000000000000L,   //wQueen    bQueen
                                 0x0010L, 0x1000000000000000L};  //wKing     bKing
 
+    //files a - h in that order
+    private long[] files = {0x0808080808080808L, 0x0404040404040404L,
+                            0x0202020202020202L, 0x0101010101010101L,
+                            0x8080808080808080L, 0x4040404040404040L,
+                            0x2020202020202020L, 0x1010101010101010L};
+
+    //ranks 1 - 8 in that order
+    private long[] ranks = {0x88L, 0x8800L, 0x880000, 0x88000000L, 0x8800000000L, 0x880000000000L,
+                            0x88000000000000L, 0x8800000000000000L};
+
     private long enPassant = 0x0L;
     private boolean[] rooksMovedFlags = {false, false, false, false}; // wARook, wHRook, bARook, bHRook
     private boolean[] kingsMovedFlags = {false, false}; //wKing, bKing
+
+    private static final long Castling_Mask = Square.A1.pos() | Square.A8.pos()
+                                            | Square.H1.pos() | Square.H8.pos()
+                                            | Square.E1.pos() | Square.E8.pos();
 //
 //    boolean wARookMoved = false;
 //    boolean wHRookMoved = false;
@@ -43,7 +57,50 @@ public class BitBoards {
 
 
     public void makeMove(Move move) {
-        //TODO: get this working
+
+        int colorIndexAdjustment = move.color().ordinal();
+        int pieceIndexAdjustment = move.pieceType().ordinal() * 2;
+
+        long fromBit = move.From().pos();
+        long toBit = move.To().pos();
+        Move.MoveType moveType = move.moveType();
+
+        PieceType promotionTo = move.promotionTo();
+        PieceType capturedPiece = move.capturedPieceType();
+
+        //this is always executed, no matter what moveType
+        int movingPieceIndex = colorIndexAdjustment + pieceIndexAdjustment;
+        bitboards[movingPieceIndex] = (bitboards[movingPieceIndex] & ~fromBit) | toBit;
+
+        switch (moveType) {
+            case CAPTURE -> capture(toBit, move.color().other().ordinal() + capturedPiece.ordinal() * 2);
+            case PROMOTION -> promotion(toBit, movingPieceIndex, colorIndexAdjustment + promotionTo.ordinal() * 2);
+            case ENPASSANT -> enPassant = 0L;
+            case KING_SIDE_CASTLE -> kingSideCastle(colorIndexAdjustment);
+            case QUEEN_SIDE_CASTLE -> {}
+            case PAWN_DOUBLE_MOVE -> {}
+            default -> {}
+        }
+    }
+
+    private void capture(long toBit, int capturedPieceIndex) {
+        bitboards[capturedPieceIndex] &= ~toBit;
+    }
+
+    private void promotion(long toBit, int movingPieceIndex, int promotionIndex) {
+        bitboards[movingPieceIndex] &= ~toBit;
+        bitboards[promotionIndex] |= toBit;
+    }
+
+    private void kingSideCastle(int colorSideAdjustment) {
+
+        bitboards[10 + colorSideAdjustment] <<= 2;
+        bitboards[6 + colorSideAdjustment] = (bitboards[6 + colorSideAdjustment] & ~files[8]) | (ranks[8 * colorSideAdjustment] & files[6]);
+    }
+
+    private void queenSideCastle(int colorSideAdjustment) {
+        bitboards[10 + colorSideAdjustment] >>>= 2;
+        bitboards[6 + colorSideAdjustment] = (bitboards[6 + colorSideAdjustment] & ~files[1]) | (ranks[8 * colorSideAdjustment] & files[4]);
     }
 
     /**
@@ -74,11 +131,22 @@ public class BitBoards {
 
         for (int i = 0; i < 6; i++) {
 
-            int index = ((color == SideColor.White)? 0 : 1) + (2 * i);
+            int index = color.ordinal() + (2 * i);
 
             occ |= bitboards[index];
         }
 
         return occ;
+    }
+
+    /**
+     * This method returns the square which can be attacked due to enPassant. EnPassant is a rule in chess, where a pawn
+     * who moved up two square, can be taken by an opposite pawn for the next turn, if and only if the pawn could have
+     * been taken if it was only moved 1 square up the board
+     *
+     * @return the Square which can be attacked due to the enPassant - rule, null if there is no such square
+     */
+    public Square getEnPassant() {
+        return Square.getSquare(enPassant);
     }
 }
--- src/Move.java
+++ src/Move.java
@@ -4,7 +4,8 @@ public record Move(
         Square From,
         Square To,
         PieceType promotionTo,
-        PieceType capturedPieceType
+        PieceType capturedPieceType,
+        MoveType moveType
 ) {
-    public enum MoveType {BASIC, CAPTURE, CASTLE, PROMOTION, ENPASSANT}
+    public enum MoveType {CAPTURE, KING_SIDE_CASTLE, QUEEN_SIDE_CASTLE, PROMOTION, PAWN_DOUBLE_MOVE, ENPASSANT}
 }
