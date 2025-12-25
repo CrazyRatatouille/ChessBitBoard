80b4ac51981ec92110fdec7af141bb853e0a00d3
Rakhman Indarbiev <rindarbie@gmail.com>
2025-12-25 18:20:49 +0100

    mailbox implemented, more expressive variable names chosen, occupancy initialisation bug fixed

--- src/BoardState.java
+++ src/BoardState.java
@@ -13,7 +13,7 @@ public class BoardState {
      * Indexing: [0,1] Pawns, [2,3] Knights, [4,5] Bishops, [6,7] Rooks, [8,9] Queens, [10,11] Kings.
      * Even indices are White, odd indices are Black.
      */
-    private long[] bitboards = {
+    private long[] pieceBB = {
             0xFF00L, 0x00FF000000000000L,
             0x0042L, 0x4200000000000000L,
             0x0024L, 0x2400000000000000L,
@@ -22,37 +22,21 @@ public class BoardState {
             0x0010L, 0x1000000000000000L
     };
 
-    private byte[] mailbox = {
-             6,  2,  4,  8, 10,  4,  2,  6,
-             0,  0,  0,  0,  0,  0,  0,  0,
+    private byte[] pieceAt = {
+            6, 2, 4, 8, 10, 4, 2, 6,
+            0, 0, 0, 0, 0, 0, 0, 0,
             -1, -1, -1, -1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1, -1, -1, -1,
-             1,  1,  1,  1,  1,  1,  1,  1,
-             7,  3,  5,  9, 11,  5,  3,  7
-    };
-
-    /**
-     * Bitmask constants for each file (A through H).
-     */
-    private static final long[] fileMasks = {
-            0x0101010101010101L, 0x0202020202020202L, 0x0404040404040404L, 0x0808080808080808L,
-            0x1010101010101010L, 0x2020202020202020L, 0x4040404040404040L, 0x8080808080808080L
-    };
-
-    /**
-     * Bitmask constants for each rank (1 through 8).
-     */
-    private static final long[] rankMasks = {
-            0xFFL, 0xFF00L, 0xFF0000, 0xFF000000L,
-            0xFF00000000L, 0xFF0000000000L, 0xFF000000000000L, 0xFF00000000000000L
+            1, 1, 1, 1, 1, 1, 1, 1,
+            7, 3, 5, 9, 11, 5, 3, 7
     };
 
     /**
      * Bitmask representing the single square available for an en passant capture.
      */
-    private long enPassant = 0x0L;
+    private long enPassantTarget = 0x0L;
 
     /**
      * bit 1 (LSB): wQueenSide <br>
@@ -60,13 +44,13 @@ public class BoardState {
      * bit 3 : bQueenSide <br>
      * bit 4 (MSB) : bKingSide
      */
-    private int castlingRights = 0xF;
+    private byte castlingRights = 0xF;
 
     /**
      * A lookup table used to update castling rights efficiently.
      * When a piece moves from or to a square, the rights are bitwise AND-ed with these values.
      */
-    private static final byte[] castlingMasks = {
+    private static final byte[] CASTLING_MASK_BY_SQUARE = {
             0xE, 0xF, 0xF, 0xF, 0xC, 0xF, 0xF, 0xD,
             0xF, 0xF, 0xF, 0xF, 0xF, 0xF, 0xF, 0xF,
             0xF, 0xF, 0xF, 0xF, 0xF, 0xF, 0xF, 0xF,
@@ -81,7 +65,7 @@ public class BoardState {
      * Occupancy bitboards.
      * Index 0: White pieces, Index 1: Black pieces, Index 2: Combined (all) pieces.
      */
-    private long[] occs = {0xFFFFL, 0xFFFF000000000000L, 0xFFFF0000000000FFL};
+    private long[] occupancy = {0xFFFFL, 0xFFFF000000000000L, 0xFFFF00000000FFFFL};
 
     /**
      * Default constructor. Initializes the board to the standard starting position.
@@ -89,18 +73,24 @@ public class BoardState {
     public BoardState() {
     }
 
+    //TODO: remove copy constructor
     /**
      * Deep-copy constructor for state branching during search.
      * * @param other The source board to copy from.
      */
     public BoardState(BoardState other) {
 
-        System.arraycopy(other.bitboards, 0, this.bitboards, 0, this.bitboards.length);
-        System.arraycopy(other.occs, 0, this.occs, 0, this.occs.length);
+        System.arraycopy(other.pieceBB, 0, this.pieceBB, 0, this.pieceBB.length);
+        System.arraycopy(other.occupancy, 0, this.occupancy, 0, this.occupancy.length);
+        System.arraycopy(other.pieceAt, 0, this.pieceAt, 0, this.pieceAt.length);
 
-        this.enPassant = other.enPassant;
+        this.enPassantTarget = other.enPassantTarget;
         this.castlingRights = other.castlingRights;
     }
+
+    /* ==========================================================================================
+                                            state updates
+     ========================================================================================== */
 
     /**
      * Executes a move on the board using a 16-bit encoded integer.
@@ -109,234 +99,192 @@ public class BoardState {
      * It packs the Source Square, Destination Square, and Move Type (promotion, capture, etc.)
      * into a single short/int for cache efficiency.
      * </p>
+     *
      * @param move The 16-bit encoded move.
      * @see <a href="https://www.chessprogramming.org/Encoding_Moves">Chess Programming Wiki: Encoding Moves</a>
      */
     public void makeMove(short move) {
 
-        //TODO: remove codeDuplication as much as possible and lift general purpose computation
-        //TODO: update mailbox
-
-        int fromSq = (move >>> 6) & 0x3F;
-        int toSq = move & 0x3F;
-
-        long fromBit = 1L << fromSq;
-        long toBit = 1L << toSq;
-        int color = mailbox[fromSq] % 2;
-
+        int from = (move >>> 6) & 0x3F;
+        int to = move & 0x3F;
         int moveType = (move & 0xF000) >>> 12;
-        int promoTo = 2 + ((moveType & 0x3) << 1);
-        int captured = mailbox[toSq];
 
-        enPassant = 0x0L;
+        long fromMask = 1L << from;
+        long toMask = 1L << to;
+        long moveMask = (fromMask | toMask);
 
-        int pIdx = mailbox[fromSq];
-        long moveMask = (fromBit | toBit);
+        int movingPiece = pieceAt[from];
+        int capturedPiece = pieceAt[to];
+        int promotionBase = 2 + ((moveType & 0x3) << 1);
+
+        int side = movingPiece & 1;
+
+        enPassantTarget = 0x0L;
 
         switch (moveType) {
-            case 0 -> quietMove(fromBit, toBit, color, pIdx);
-            case 1 -> pawnDoubleMove(fromBit, toBit, color, pIdx);
-            case 2 -> kingSideCastle(fromBit, toBit, color, pIdx);
-            case 3 -> queenSideCastle(fromBit, toBit, color, pIdx);
-            case 4 -> capture(fromBit, toBit, color, captured, pIdx);
-            case 5 -> enPassant(fromBit, toBit, color, pIdx);
 
-            case 8, 9, 10, 11-> promotion(fromBit, toBit, color, promoTo, pIdx);
+            case 0 -> quietMove(to, movingPiece, moveMask, side);
+            case 1 -> pawnDoubleMove(to, toMask, moveMask, movingPiece, side);
+            case 2 -> kingSideCastle(to, moveMask, movingPiece, side);
+            case 3 -> queenSideCastle(to, moveMask, movingPiece, side);
+            case 4 -> capture(to, fromMask, toMask, moveMask, movingPiece, capturedPiece, side);
+            case 5 -> enPassant(to, fromMask, toMask, movingPiece, side);
 
-            case 12, 13, 14, 15 -> promotionAndCapture(fromBit, toBit, color, captured, promoTo, pIdx);
+            case 8, 9, 10, 11 -> promotion(to, fromMask, toMask, moveMask, movingPiece, promotionBase, side);
+
+            case 12, 13, 14, 15 -> promotionAndCapture(to, fromMask, toMask, moveMask, movingPiece, capturedPiece, promotionBase, side);
         }
 
-        updateCastlingRights(fromSq, toSq);
+        pieceAt[from] = -1;
+
+        updateCastlingRights(from, to);
+
+//        assert((occs[0] | occs[1]) == occs[2]);
+//        assert((occs[0] & occs[1]) == 0);
+//        assert(bitboards[10] != 0 && bitboards[11] != 0);
     }
 
-    /**
-     * Handles non-capturing, non-special moves using XOR toggling.
-     *
-     * @param fromBit Bitmask of the source square.
-     * @param toBit   Bitmask of the destination square.
-     * @param color   Ordinal of the moving side.
-     * @param pIdx    Index of the specific piece bitboard.
-     */
-    private void quietMove(long fromBit, long toBit, int color, int pIdx) {
+    private void quietMove(int to, int movingPiece, long moveMask, int side) {
 
-        long moveMask = (fromBit | toBit);
+        pieceBB[movingPiece] ^= moveMask;
 
-        bitboards[pIdx] ^= moveMask;
-        occs[color] ^= moveMask;
-        occs[2] ^= moveMask;
+        pieceAt[to] = (byte)movingPiece;
+
+        occupancy[side] ^= moveMask;
+        occupancy[2] ^= moveMask;
     }
 
-    /**
-     * Executes a capture move by clearing the victim's bit.
-     *
-     * @param fromBit  Bitmask of the source square.
-     * @param toBit    Bitmask of the destination square.
-     * @param color    Ordinal of the moving side.
-     * @param captured Ordinal of the captured piece type.
-     * @param pIdx     Index of the moving piece bitboard.
-     */
-    private void capture(long fromBit, long toBit, int color, int captured, int pIdx) {
+    private void capture(int to, long fromMask, long toMask, long moveMask, int movingPiece, int capturedPiece, int side) {
 
-        long moveMask = (fromBit | toBit);
+        pieceBB[movingPiece] ^= moveMask;
+        pieceBB[capturedPiece] ^= toMask;
 
-        bitboards[pIdx] ^= (fromBit | toBit);
-        bitboards[captured] ^= toBit;
+        pieceAt[to] = (byte)movingPiece;
 
-        occs[color] ^= moveMask;
-        occs[0x1 ^ color] ^= toBit;
-        occs[2] ^= fromBit;
+        occupancy[side] ^= moveMask;
+        occupancy[0x1 ^ side] ^= toMask;
+        occupancy[2] ^= fromMask;
     }
 
-    /**
-     * Handles pawn promotion by swapping the pawn bit for a new piece bit.
-     *
-     * @param fromBit Bitmask of the source square.
-     * @param toBit   Bitmask of the destination square.
-     * @param color   Ordinal of the moving side.
-     * @param promoTo Ordinal of the piece type being promoted to.
-     * @param pIdx    Index of the pawn bitboard.
-     */
-    private void promotion(long fromBit, long toBit, int color, int promoTo, int pIdx) {
+    private void promotion(int to, long fromMask, long toMask, long moveMask, int movingPiece, int promotedPiece, int side) {
 
-        long moveMask = (fromBit | toBit);
+        byte promoIdx = (byte)(side + promotedPiece);
 
-        bitboards[pIdx] ^= fromBit;
-        bitboards[color + promoTo] ^= toBit;
+        pieceBB[movingPiece] ^= fromMask;
+        pieceBB[promoIdx] ^= toMask;
 
-        occs[color] ^= moveMask;
-        occs[2] ^= moveMask;
+        pieceAt[to] = promoIdx;
+
+        occupancy[side] ^= moveMask;
+        occupancy[2] ^= moveMask;
     }
 
-    /**
-     * Executes a move that both captures a piece and promotes a pawn.
-     *
-     * @param fromBit  Bitmask of the source square.
-     * @param toBit    Bitmask of the destination square.
-     * @param color    Ordinal of the moving side.
-     * @param captured Ordinal of the captured piece type.
-     * @param promoTo  Ordinal of the piece type being promoted to.
-     * @param pIdx     Index of the pawn bitboard.
-     */
-    private void promotionAndCapture(long fromBit, long toBit, int color, int captured, int promoTo, int pIdx) {
+    private void promotionAndCapture(int to, long fromMask, long toMask, long moveMask, int movingPiece
+            , int capturedPiece, int promotedPiece, int side) {
 
-        bitboards[pIdx] ^= fromBit;
-        bitboards[color + promoTo] ^= toBit;
-        bitboards[captured] ^= toBit;
+        byte promoIdx = (byte)(side + promotedPiece);
 
-        occs[color] ^= (fromBit | toBit);
-        occs[0x1 ^ color] ^= toBit;
-        occs[2] ^= fromBit;
+        pieceBB[movingPiece] ^= fromMask;
+        pieceBB[promoIdx] ^= toMask;
+        pieceBB[capturedPiece] ^= toMask;
+
+        pieceAt[to] = promoIdx;
+
+        occupancy[side] ^= moveMask;
+        occupancy[0x1 ^ side] ^= toMask;
+        occupancy[2] ^= fromMask;
     }
 
-    /**
-     * Updates bitboards for a King-side castle, including the Rook shift.
-     *
-     * @param fromBit Bitmask of the King's start square.
-     * @param toBit   Bitmask of the King's end square.
-     * @param color   Ordinal of the moving side.
-     * @param pIdx    Index of the King bitboard.
-     */
-    private void kingSideCastle(long fromBit, long toBit, int color, int pIdx) {
+    private void kingSideCastle(int to, long moveMaskKing, int movingPiece, int side) {
 
-        int rookIndex = 6 + color;
-        long rookPos = Square.H1.pos() << (color * 56);
+        byte rookIndex = (byte)(6 + side);
+        long rookPos = Square.H1.pos() << (side * 56);
 
-        long moveMaskK = (fromBit | toBit);
         long moveMaskR = (rookPos | rookPos >>> 2);
-        long moveMaskCombined = (moveMaskK | moveMaskR);
+        long moveMaskCombined = (moveMaskKing | moveMaskR);
 
-        bitboards[pIdx] ^= moveMaskK;
-        bitboards[rookIndex] ^= moveMaskR;
+        pieceBB[movingPiece] ^= moveMaskKing;
+        pieceBB[rookIndex] ^= moveMaskR;
 
-        occs[color] ^= moveMaskCombined;
-        occs[2] ^= moveMaskCombined;
+        pieceAt[to] = (byte)(movingPiece);
+        pieceAt[to - 1] = rookIndex;
+        pieceAt[to + 1] = -1;
+
+        occupancy[side] ^= moveMaskCombined;
+        occupancy[2] ^= moveMaskCombined;
     }
 
-    /**
-     * Updates bitboards for a Queen-side castle, including the Rook shift.
-     *
-     * @param fromBit Bitmask of the King's start square.
-     * @param toBit   Bitmask of the King's end square.
-     * @param color   Ordinal of the moving side.
-     * @param pIdx    Index of the King bitboard.
-     */
-    private void queenSideCastle(long fromBit, long toBit, int color, int pIdx) {
+    private void queenSideCastle(int to, long moveMaskKing, int movingPiece, int side) {
 
-        int rookIndex = 6 + color;
-        long rookPos = Square.A1.pos() << (color * 56);
+        byte rookIndex = (byte)(6 + side);
+        long rookPos = Square.A1.pos() << (side * 56);
 
-        long moveMaskK = (fromBit | toBit);
         long moveMaskR = (rookPos | rookPos << 3);
-        long moveMaskCombined = (moveMaskK | moveMaskR);
+        long moveMaskCombined = (moveMaskKing | moveMaskR);
 
-        bitboards[pIdx] ^= moveMaskK;
-        bitboards[rookIndex] ^= moveMaskR;
+        pieceBB[movingPiece] ^= moveMaskKing;
+        pieceBB[rookIndex] ^= moveMaskR;
 
-        occs[color] ^= moveMaskCombined;
-        occs[2] ^= moveMaskCombined;
+        pieceAt[to] = (byte)(movingPiece);
+        pieceAt[to + 1] = rookIndex;
+        pieceAt[to - 2] = -1;
+
+        occupancy[side] ^= moveMaskCombined;
+        occupancy[2] ^= moveMaskCombined;
     }
 
-    /**
-     * Moves a pawn two squares forward and sets the en passant target square.
-     *
-     * @param fromBit Bitmask of the pawn's start square.
-     * @param toBit   Bitmask of the pawn's end square.
-     * @param color   Ordinal of the moving side.
-     * @param pIdx    Index of the pawn bitboard.
-     */
-    private void pawnDoubleMove(long fromBit, long toBit, int color, int pIdx) {
+    private void pawnDoubleMove(int to, long toMask, long moveMask, int movingPiece,  int side) {
 
-        quietMove(fromBit, toBit, color, pIdx);
+        quietMove(to, movingPiece, moveMask, side);
 
-        switch (color) {
-            case 0 -> enPassant = toBit >>> 8;
-            case 1 -> enPassant = toBit << 8;
+        switch (side) {
+            case 0 -> enPassantTarget = toMask >>> 8;
+            case 1 -> enPassantTarget = toMask << 8;
         }
     }
 
-    /**
-     * Executes an en passant capture, removing the opponent's pawn from the rank behind.
-     *
-     * @param fromBit Bitmask of the pawn's start square.
-     * @param toBit   Bitmask of the pawn's end square.
-     * @param color   Ordinal of the moving side.
-     * @param pIdx    Index of the moving pawn bitboard.
-     */
-    private void enPassant(long fromBit, long toBit, int color, int pIdx) {
+    private void enPassant(int to, long fromMask, long toMask, int movingPiece, int side) {
 
-        int capColor = 0x1 ^ color;
-        long moveMask = (fromBit | toBit);
-        long capBit = toBit;
+        int capColor = 0x1 ^ side;
+        long moveMask = (fromMask | toMask);
+        long capBit = toMask;
 
-        switch (color) {
-            case 0 -> capBit >>>= 8;
-            case 1 -> capBit <<= 8;
+        switch (side) {
+            case 0 -> {
+                capBit >>>= 8;
+                pieceAt[to - 8] = -1;
+            }
+            case 1 -> {
+                capBit <<= 8;
+                pieceAt[to + 8] = -1;
+            }
         }
 
-        bitboards[pIdx] ^= moveMask;
-        bitboards[capColor] ^= capBit;
+        pieceBB[movingPiece] ^= moveMask;
+        pieceBB[capColor] ^= capBit;
 
-        occs[color] ^= moveMask;
-        occs[capColor] ^= capBit;
-        occs[2] ^= (moveMask | capBit);
+        pieceAt[to] = (byte)movingPiece;
+
+        occupancy[side] ^= moveMask;
+        occupancy[capColor] ^= capBit;
+        occupancy[2] ^= (moveMask | capBit);
     }
 
-    /**
-     * Updates the global castling rights by checking both source and destination squares.
-     *
-     * @param from The source square of the move.
-     * @param to   The destination square of the move.
-     */
     private void updateCastlingRights(int from, int to) {
-        castlingRights &= (castlingMasks[from] & castlingMasks[to]);
+        castlingRights &= (CASTLING_MASK_BY_SQUARE[from] & CASTLING_MASK_BY_SQUARE[to]);
     }
+
+    /* ==========================================================================================
+                                            state getters
+     ========================================================================================== */
 
     /**
      * Retrieves the occupancy mask for the entire board.
+     *
      * @return A bitboard of all pieces.
      */
     public long getOccupancy() {
-        return occs[2];
+        return occupancy[2];
     }
 
     /**
@@ -346,7 +294,7 @@ public class BoardState {
      * @return A bitboard of all pieces belonging to the specified color.
      */
     public long getColorOccupancy(SideColor color) {
-        return occs[color.ordinal()];
+        return occupancy[color.ordinal()];
     }
 
     /**
@@ -354,11 +302,11 @@ public class BoardState {
      *
      * @return The target Square, or null if en passant is not available.
      */
-    public Square getEnPassant() {
-        return Square.getSquare(enPassant);
+    public Square getEnPassantTarget() {
+        return Square.getSquare(enPassantTarget);
     }
 
     public long getPieces(int i) {
-        return bitboards[i];
+        return pieceBB[i];
     }
 }
